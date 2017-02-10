0e839235d3ee77158de957a8749a47919bc4e91e
Duke Cheng <dk@feinian.me>
2017-02-11 00:13:58 +0800

    account controller refactor

--- src/DotNetLive.Web/Controllers/AccountController.cs
+++ src/DotNetLive.Web/Controllers/AccountController.cs
@@ -1,54 +1,16 @@
 using DotNetLive.Framework.UserIdentity;
+using DotNetLive.Framework.WebFramework.Controllers;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using System.Net;
 
 namespace DotNetLive.Web.Controllers
 {
     [Authorize]
-    public class AccountController : Controller
+    public class AccountController : BaseAccountController
     {
-        private SecuritySettings _securitySettings;
-
-        public AccountController(IOptions<SecuritySettings> securitySetting)
+        public AccountController(IOptions<SecuritySettings> securitySetting) : base(securitySetting)
         {
-            this._securitySettings = securitySetting.Value;
-        }
 
-        [AllowAnonymous]
-        public ActionResult Login(string userNameOrEmailAddress = "", string returnUrl = "", string successMessage = "")
-        {
-            return Redirect($"{LoginUrl}/account/login?returnUrl={ GetEncodeReturlUrl(returnUrl)}");
         }
-
-        private string GetEncodeReturlUrl(string returnUrl)
-        {
-            var context = Request.HttpContext;
-            //@($"{Context.Request.Scheme}://{Context.Request.Host}{Context.Request.Path}{Context.Request.QueryString}")
-            return WebUtility.UrlEncode($"{context.Request.Scheme}://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}");
-        }
-
-        [AllowAnonymous]
-        public ActionResult Register(string returnUrl)
-        {
-            return Redirect($"{LoginUrl}/account/register?returnUrl={ GetEncodeReturlUrl(returnUrl)}");
-        }
-
-        [Authorize]
-        public ActionResult LogOff(string returnUrl = "")
-        {
-            return Redirect($"{LoginUrl}/account/logoff?returnUrl={GetEncodeReturlUrl(returnUrl)}");
-        }
-
-        [Authorize]
-        public ActionResult Manager()
-        {
-            return Redirect($"{LoginUrl}/manager");
-        }
-
-        #region Util
-        public string LoginUrl => _securitySettings.LoginUrl.TrimEnd('/');
-        #endregion
     }
 }
