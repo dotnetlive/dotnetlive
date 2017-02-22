29d05a010ed5f8d1c88127412695c883e0139156
Duke Cheng <duke@Dukes-MacBook-Pro.local>
2017-02-23 00:40:34 +0800

    update config

--- deploy/config.sh
+++ deploy/config.sh
@@ -1,3 +1,9 @@
 ## scan current dnl service
 ## install all dnl service
 ## update nginx config
+
+mkdir /data && echo "/dev/sdc  /data   ext4  defaults 1 1" >> /etc/fstab
+
+
+cp kestrel-dotnetlive-* /etc/systemd/system
+find kestrel-dotnetlive-*.service -exec systemctl enable {} \;
